9313230a3d52220fef521d555d32b2707d768756
Amol Saurav <amolsaurav20045@gmail.com>
2026-04-27 10:52:19 +0530

    Implement subset generation function in subsets.cpp

---
+++ End_Sem_Practical_Krg-2B_23BCS10067/subsets.cpp
@@ -0,0 +1,14 @@
+vector<vector<int>>subset(vector<int>&nums){
+    vector<vector<int>>ans;
+    int n = nums.size();
+    for(int i=0;i<(1<<n);i++){
+        vector<int>temp;
+        for(int j=0;j<n;j++){
+            if(i&(1<<j)){
+                temp.push_back(nums[j]);
+            }
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
