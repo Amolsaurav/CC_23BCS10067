386fec059096df10ff2be09e952e6f55b820276b
23bcs10067-afk <23bcs10067@gmail.com>
2026-08-07 11:06:56 +0530

    Update triangular_sum.cpp

--- Experiment 1/triangular_sum.cpp
+++ Experiment 1/triangular_sum.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        while(nums.size()>1){
-            vector<int>temp(nums.size()-1);
+        int n = nums.size();
+        if(n==1) return nums[0];
+        while(n>1){
+            vector<int>temp(n-1);
             for(int i=0;i<nums.size()-1;i++){
                 temp[i]=(nums[i]+nums[i+1])%10;
             }
